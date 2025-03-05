81f0d8c8079d35b62d5593ac0207cc7095397e77
showstarpro <1359947339@qq.com>
2025-03-05 14:24:12 +0000

    update training script

--- DiT/scripts/train_dit_contrastive.sh
+++ DiT/scripts/train_dit_contrastive.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun \
   --nnodes=1 \
-  --nproc_per_node=4 \
+  --nproc_per_node=8 \
   train_contrastive.py \
   --model DiT-XL/2 \
   --num-classes 10 \
