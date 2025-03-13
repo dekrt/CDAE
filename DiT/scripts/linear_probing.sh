0722b39ab9c8c39db96fc6a10d090b7d1c543063
dekrt <96043667+dekrt@users.noreply.github.com>
2025-03-13 21:23:06 +0800

    Update linear_probing.sh

--- DiT/scripts/linear_probing.sh
+++ DiT/scripts/linear_probing.sh
@@ -6,6 +6,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun \
   --val-data-path /lpai/dataset/imagenet-1k/0-1-0/ILSVRC2012/val \
   --dataset imagenet \
   --batch-size 256 \
-  --ckpt /lpai/inputs/models/ditssl-25-03-02-1/cdae/pretrained_models/DiT-XL-2-256x256.pt \
+  --ckpt /lpai/output/models/results/000-DiT-XL-2/checkpoints/0300000.pt \
+  # --ckpt /lpai/inputs/models/ditssl-25-03-02-1/cdae/pretrained_models/DiT-XL-2-256x256.pt \
   # --ckpt /lpai/zxk/ddae/DiT/pretrained_models/DiT-XL-2-256x256.pt \
   # --use-amp \
